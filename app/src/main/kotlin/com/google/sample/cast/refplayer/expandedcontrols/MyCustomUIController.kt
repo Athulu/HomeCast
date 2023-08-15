2cb03d7b7386917bd0ddf11af2f8853ea9ae56a0
Athulu <nikodem1703@gmail.com>
2023-08-15 19:16:28 +0200

    dodanie przewijania do przodu i tyłu o 30s

---
+++ app/src/main/kotlin/com/google/sample/cast/refplayer/expandedcontrols/MyCustomUIController.kt
@@ -0,0 +1,22 @@
+package com.google.sample.cast.refplayer.expandedcontrols
+
+import android.view.View
+import android.widget.ImageView
+import com.google.android.gms.cast.framework.media.uicontroller.UIController
+
+class MyCustomUIController(private val mView: View) : UIController() {
+
+    override fun onMediaStatusUpdated() {
+        mView.isEnabled = true
+    }
+
+    fun updateButtonImage(imageResId: Int) {
+        if (mView is ImageView) {
+            mView.setImageResource(imageResId)
+        }
+    }
+
+    override fun onSendingRemoteMediaRequest() {
+        super.onSendingRemoteMediaRequest()
+    }
+}
